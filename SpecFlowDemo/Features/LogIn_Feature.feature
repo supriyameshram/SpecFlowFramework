a7b93627bc806489b6b81795da241344a1032518
Supriya Meshram <supriya_meshram@persistent.co.in>
2019-02-27 16:37:07 +0530

    Second Commit

---
+++ SpecFlowDemo/Features/LogIn_Feature.feature
@@ -0,0 +1,17 @@
+﻿Feature: LogIn_Feature
+In order to access my account
+	As a user of the website
+	I want to log into the website
+
+@mytag
+Scenario: Successful Login with Valid Credentials
+ Given User is on LogIn Page
+ When User enter credentials
+ | Key      | Value  |
+ | Username | mgr123 |
+ | Password | mgr!23 |
+ And Click on LogIn button
+ Then Successful LogIn message should display
+ When User Click on LogOut button
+ Then Successful Logout message should display
+ 
---
+++ SpecFlowDemo/Steps/LogIn_FeatureSteps.cs
@@ -0,0 +1,60 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using SpecFlowDemo.Utils;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowDemo.Steps
+{
+    [Binding]
+    public class LogIn_FeatureSteps
+    {
+        private IWebDriver driver;
+       [Given(@"User is on LogIn Page")]
+        public void GivenUserIsOnLogInPage()
+        {  
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            driver.Url = "http://demo.guru99.com/v4/";
+            
+        }
+
+        [When(@"User enter credentials")]
+        public void WhenUserEnterCredentials(Table table)
+        {
+            var dictionary = TableExtensions.ToDictionary(table);
+            var test = dictionary["Username"];
+            var test1 = dictionary["Password"];
+            driver.FindElement(By.Name("uid")).SendKeys(dictionary["Username"]);
+            driver.FindElement(By.Name("password")).SendKeys(dictionary["Password"]);
+        }
+
+
+        [When(@"Click on LogIn button")]
+        public void WhenClickOnLogInButton()
+        {
+            driver.FindElement(By.Name("btnLogin")).Click();
+        }
+
+
+        [Then(@"Successful LogIn message should display")]
+        public void ThenSuccessfulLogInMessageShouldDisplay()
+        {
+            Console.WriteLine("Login Successfully");
+        }
+
+        [When(@"User Click on LogOut button")]
+        public void WhenUserLogOutFromTheApplication()
+        {
+            driver.FindElement(By.LinkText("Log out")).Click();
+            driver.Close();
+        }
+      
+        [Then(@"Successful Logout message should display")]
+        public void ThenSuccessfulLogoutMessageShouldDisplay()
+        {
+            Console.WriteLine("Successful Logout");
+        }
+    }
+}
